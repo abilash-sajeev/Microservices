e676250c1afcfd8efd297d9285ac44da7d7dc1e3
abilash-sajeev <abhilashsajeev1999@gmail.com>
2021-11-25 18:39:26 +0530

    Implemented circuit breaker

---
+++ Abilash Sajeev/store/src/main/java/com/examples/store/exceptions/AccessDeniedException.java
@@ -0,0 +1,29 @@
+package com.examples.store.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class AccessDeniedException extends RuntimeException {
+
+	private String message;
+	private HttpStatus status;
+
+	public AccessDeniedException() {
+		super();
+		this.message = "You don't have permission to access / on this server";
+		this.status = HttpStatus.FORBIDDEN;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+	
+	@Override
+	public String toString() {
+		return status + ": " +  message;
+	}
+	
+}
