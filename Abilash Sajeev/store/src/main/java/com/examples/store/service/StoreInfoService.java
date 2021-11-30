4451ddc120917faa16f9ea14b946ee89ec178858
abilash-sajeev <abhilashsajeev1999@gmail.com>
2021-11-30 19:11:47 +0530

    Implemented API Gateway & routed all traffic through it

--- Abilash Sajeev/store/src/main/java/com/examples/store/service/StoreInfoService.java
+++ Abilash Sajeev/store/src/main/java/com/examples/store/service/StoreInfoService.java
@@ -6,10 +6,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.ctc.wstx.shaded.msv_core.util.Uri;
 import com.examples.store.models.Product;
 import com.examples.store.models.ProductData;
 
@@ -21,9 +23,15 @@ public class StoreInfoService {
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@Value("${product.info.url}")
+	private String url;
+	
+	@Value("${admin.console.url}")
+	private String adminUrl;
+	
 	@CircuitBreaker(name = "store-api", fallbackMethod = "getFallbackProducts")
 	public List<ProductData> getProducts(){
-		Product[] productsArray = restTemplate.getForObject("http://product-info/GetProductInfo", Product[].class);
+		Product[] productsArray = restTemplate.getForObject(url, Product[].class);
 		return Arrays.asList(productsArray).stream()
 				.map(product -> new ProductData(product.getId(), product.getProductName(), product.getPrice(), "Unsold"))
 				.collect(Collectors.toList());
@@ -31,12 +39,13 @@ public class StoreInfoService {
 	
 	@CircuitBreaker(name = "store-api", fallbackMethod = "getFallbackProduct")
 	public ProductData getProduct(String id) {
-		Product product = restTemplate.getForObject("http://product-info/GetProductInfo/"+id, Product.class);
+		Product product = restTemplate.getForObject(url+id, Product.class);
 		return new ProductData(product.getId(), product.getProductName(), product.getPrice(), "Unsold");
 	}
 
 	public ResponseEntity<String> admin() {
-		ResponseEntity<String> response = restTemplate.getForEntity("http://product-info/Admin", String.class);
+		System.out.println(adminUrl);
+		ResponseEntity<String> response = restTemplate.getForEntity(adminUrl, String.class);
 		return response;
 	}
 	
